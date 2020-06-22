9ee07cbde583aca04ba40dd0166f48e60ee66bbb
thenamskov <thenamskov@localhost.localdomain>
2020-06-22 08:21:48 -0500

    update: mobile conform

--- script/index.js
+++ script/index.js
@@ -3,8 +3,18 @@ console.log(headerLinks);
 
 headerLinks.forEach(headerLink => {
         let toggleLinks = false;
+        const sublinks = headerLink.children[1];
+        const chevronIconElement = headerLink.children[0].firstChild;
         headerLink.addEventListener('click', e => {
             toggleLinks = !toggleLinks;
-            toggleLinks ? headerLink.children[1].style.display = 'block' : headerLink.children[1].style.display = 'none';
+            if(toggleLinks) {
+                sublinks.style.display = 'block'
+                chevronIconElement.classList.add('fa-chevron-up');
+                chevronIconElement.classList.remove('fa-chevron-down')
+            } else {
+                sublinks.style.display = 'none';
+                chevronIconElement.classList.remove('fa-chevron-up');
+                chevronIconElement.classList.add('fa-chevron-down')
+            }
     })
 })
