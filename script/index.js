87070df1ffc3ded237c4dedb9d588d0248dae044
thenamskov <thenamskov@localhost.localdomain>
2020-07-05 10:22:06 -0500

    update: eventListener fix

--- script/index.js
+++ script/index.js
@@ -1,26 +1,6 @@
 const headerLink = document.querySelector('.menu');
 const headerLinks = document.querySelectorAll('.header-link');
 
-const menuTrigger = document.getElementById('menu-toggler');
-
-console.log(menuTrigger, headerLink);
-
-let menuIsTriggered = false;
-menuTrigger.addEventListener('click', e => {
-
-    console.log("clicked!");
-    menuIsTriggered = !menuIsTriggered;
-    console.log(menuIsTriggered);
-    if(menuIsTriggered) {
-        headerLink.style.display = 'block';
-        headerLink.classList.toggle('.header-links');
-    } else {
-        headerLink.classList.toggle('.header-links');
-        headerLink.style.display = 'none';
-    }
-    
-})
-
 headerLinks.forEach(headerLink => {
         let toggleLinks = false;
         const sublinks = headerLink.children[1];
